8b2796bcb298c39c8633279b3141a7289aad8a8f
myrsa-viraj <viraj@myrsa.net>
2021-08-05 23:33:56 +0530

    secondCommit

--- conf.js
+++ conf.js
@@ -1,7 +1,10 @@
 /* eslint-disable no-undef */
+let SpecReporter = require('jasmine-spec-reporter').SpecReporter
 
 exports.config = {
+  allScriptsTimeout:11000,
   directConnect: true,
+  SELENIUM_PROMISE_MANAGER:false,
 
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
@@ -18,5 +21,15 @@ exports.config = {
   // Options to be passed to Jasmine.
   jasmineNodeOpts: {
     defaultTimeoutInterval: 999999
-  }
+  },
+
+  onPrepare: function () {
+    jasmine.getEnv().addReporter(
+      new SpecReporter({
+        spec: {
+          displayStacktrace: true,
+        },
+      })
+    )
+  },
 };
